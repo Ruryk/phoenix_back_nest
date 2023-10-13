7691448c8c38446bd2c9ab9d333034ee0eb405c1
valentin-gerasin <gerasin1001@gmail.com>
2023-10-13 14:06:16 +0300

    alternative authentication created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserAuthModule } from './user-auth/user-auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://Val:val@cluster0.cnub6md.mongodb.net/phoenix',
+    ),
+    UserAuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
